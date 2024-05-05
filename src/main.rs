fa630fc09e2f36f4a9bf403394ca7cbe03733963
Gian Calgeer <gidoca@gmail.com>
2024-05-05 20:32:01 +0200

    Add pairs and triplees

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,13 @@ enum RegularCardValue {
     Two = 2,
 }
 
-#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+impl RegularCardValue {
+    fn numeric_value(&self) -> isize {
+        *self as isize
+    }
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Copy, Clone)]
 enum RegularCardSuite {
     Heart,
     Diamond,
@@ -23,7 +29,7 @@ enum RegularCardSuite {
     Clubs,
 }
 
-#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Copy, Clone)]
 enum SpecialCardType {
     Dragon,
     Phoenix,
@@ -31,10 +37,63 @@ enum SpecialCardType {
     Dog,
 }
 
-#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Copy, Clone)]
 enum Card {
     RegularCard(RegularCardValue, RegularCardSuite),
     SpecialCard(SpecialCardType),
+}
+
+impl Card {
+    fn can_be_played_on_top_of_single_card(&self, other: &Card) -> bool {
+        match (self, other) {
+            (Card::RegularCard(self_value, _), Card::RegularCard(other_value, _)) => {
+                self_value > other_value
+            }
+
+            (Card::SpecialCard(SpecialCardType::Dragon), _) => true,
+
+            (Card::SpecialCard(SpecialCardType::Phoenix), Card::RegularCard(_, _)) => true,
+            (Card::RegularCard(_, _), Card::SpecialCard(SpecialCardType::Phoenix)) => true,
+            (
+                Card::SpecialCard(SpecialCardType::Phoenix),
+                Card::SpecialCard(SpecialCardType::One),
+            ) => true,
+            (
+                Card::SpecialCard(SpecialCardType::Phoenix),
+                Card::SpecialCard(SpecialCardType::Dog),
+            ) => true,
+            (
+                Card::SpecialCard(SpecialCardType::Phoenix),
+                Card::SpecialCard(SpecialCardType::Dragon),
+            ) => false,
+
+            (Card::SpecialCard(SpecialCardType::One), Card::SpecialCard(SpecialCardType::Dog)) => {
+                true
+            }
+            (Card::SpecialCard(SpecialCardType::One), _) => false,
+
+            (Card::SpecialCard(SpecialCardType::Dog), _) => false,
+
+            // Any other combination is covered by the reverse. This works because special cards
+            // can only occur once.
+            (this_card @ Card::RegularCard(_, _), other_card) => {
+                !other_card.can_be_played_on_top_of_single_card(this_card)
+            }
+
+            _ => panic!(),
+        }
+    }
+
+    fn score(&self) -> i8 {
+        match self {
+            Card::RegularCard(RegularCardValue::King, _) => 10,
+            Card::RegularCard(RegularCardValue::Ten, _) => 10,
+            Card::RegularCard(RegularCardValue::Five, _) => 5,
+            Card::SpecialCard(SpecialCardType::Dragon) => 25,
+            Card::SpecialCard(SpecialCardType::Phoenix) => -25,
+            _ => 0,
+        }
+    }
 }
 
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
@@ -47,40 +106,50 @@ enum HandType {
     FullHouse,
 }
 
-impl Card {
-    fn can_be_played_on_top_of_single_card(&self, other: &Card) -> bool {
-        match (self, other) {
-            (Card::RegularCard(self_value, _), Card::RegularCard(other_value ,_)) => self_value > other_value,
+struct Hand(Vec<Card>);
 
-            (Card::SpecialCard(SpecialCardType::Dragon), _) => true,
+impl Hand {
+    fn new(mut cards: Vec<Card>) -> Hand {
+        cards.sort_unstable();
+        Hand(cards)
+    }
 
-            (Card::SpecialCard(SpecialCardType::Phoenix), Card::RegularCard(_, _)) => true,
-            (Card::RegularCard(_, _), Card::SpecialCard(SpecialCardType::Phoenix)) => true,
-            (Card::SpecialCard(SpecialCardType::Phoenix), Card::SpecialCard(SpecialCardType::One)) => true,
-            (Card::SpecialCard(SpecialCardType::Phoenix), Card::SpecialCard(SpecialCardType::Dog)) => true,
-            (Card::SpecialCard(SpecialCardType::Phoenix), Card::SpecialCard(SpecialCardType::Dragon)) => false,
-
-            (Card::SpecialCard(SpecialCardType::One), Card::SpecialCard(SpecialCardType::Dog)) => true,
-            (Card::SpecialCard(SpecialCardType::One), _) => false,
-
-            (Card::SpecialCard(SpecialCardType::Dog), _) => false,
-
-            // Any other combination is covered by the reverse. This works because special cards
-            // can only occur once.
-            (this_card @ Card::RegularCard(value, color), other_card) => !other_card.can_be_played_on_top_of_single_card(this_card),
-
-            _ => panic!()
+    fn hand_type(&self) -> Option<HandType> {
+        match self {
+            hand if hand.is_valid_single_card() => Some(HandType::SingleCard),
+            hand if hand.is_valid_pair() => Some(HandType::Pair),
+            hand if hand.is_valid_triple() => Some(HandType::Triple),
+            _ => None,
         }
     }
 
-    fn score(&self) -> i8 {
-        match(self) {
-            Card::RegularCard(RegularCardValue::King, _) => 10,
-            Card::RegularCard(RegularCardValue::Ten, _) => 10,
-            Card::RegularCard(RegularCardValue::Five, _) => 5,
-            Card::SpecialCard(SpecialCardType::Dragon) => 25,
-            Card::SpecialCard(SpecialCardType::Phoenix) => -25,
-            _ => 0,
+    fn is_valid_single_card(&self) -> bool {
+        self.0.len() == 1
+    }
+
+    fn is_valid_pair(&self) -> bool {
+        match self.0.as_slice() {
+            [Card::RegularCard(value1, _), Card::RegularCard(value2, _)] if value1 == value2 => {
+                true
+            }
+            [Card::RegularCard(_, _), Card::SpecialCard(SpecialCardType::Phoenix)] => true,
+            _ => false,
+        }
+    }
+
+    fn is_valid_triple(&self) -> bool {
+        match self.0.as_slice() {
+            [Card::RegularCard(value1, _), Card::RegularCard(value2, _), Card::RegularCard(value3, _)]
+                if value1 == value2 && value2 == value3 =>
+            {
+                true
+            }
+            [Card::RegularCard(value1, _), Card::RegularCard(value2, _), Card::SpecialCard(SpecialCardType::Phoenix)]
+                if value1 == value2 =>
+            {
+                true
+            }
+            _ => false,
         }
     }
 }
@@ -89,7 +158,23 @@ fn main() {
     let card1 = Card::RegularCard(RegularCardValue::King, RegularCardSuite::Heart);
     let card2 = Card::RegularCard(RegularCardValue::Four, RegularCardSuite::Clubs);
     let card_one = Card::SpecialCard(SpecialCardType::One);
-    println!("{:?} can be played on top of {:?}: {:?}", card1, card2, card1.can_be_played_on_top_of_single_card(&card2));
-    println!("{:?} can be played on top of {:?}: {:?}", card_one, card2, card_one.can_be_played_on_top_of_single_card(&card2));
+    println!(
+        "{:?} can be played on top of {:?}: {:?}",
+        card1,
+        card2,
+        card1.can_be_played_on_top_of_single_card(&card2)
+    );
+    println!(
+        "{:?} can be played on top of {:?}: {:?}",
+        card_one,
+        card2,
+        card_one.can_be_played_on_top_of_single_card(&card2)
+    );
+    println!(
+        "{:?} can be played on top of {:?}: {:?}",
+        card2,
+        card_one,
+        card2.can_be_played_on_top_of_single_card(&card_one)
+    );
     println!("{:?} scores {:?} points", card1, card1.score());
 }
