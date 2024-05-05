8e6cebf058f65b06e320949ea18a054e2c8023cc
Gian Calgeer <gidoca@gmail.com>
2024-05-05 17:10:39 +0200

    Add regular card value enum

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+enum RegularCardValue {
+    Ace,
+    King,
+    Queen,
+    Jack,
+    Ten,
+    Nine,
+    Eight,
+    Seven,
+    Six,
+    Five,
+    Four,
+    Three,
+    Two,
+}
+
+fn main() {
+}
